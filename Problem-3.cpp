0d1ede5ca4c261da53f87035c746b81bad0f4fda
soumayagrawal1234 <145331076+soumayagrawal1234@users.noreply.github.com>
2025-05-21 10:46:52 +0530

    Update Problem-3.cpp

--- Problem-3.cpp
+++ Problem-3.cpp
@@ -1,1 +1,26 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    int x;
+    if(a%2==0){
+      x=a-1;
+    } 
+    else{
+      x=a;
+    }
+    for (int i = 1; i <= x; i++) {
+        cout << (2 * i - 1);
+        if (i != x) {
+            cout << ", ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}

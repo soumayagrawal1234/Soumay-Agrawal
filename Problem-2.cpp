d8daa415e22f8608ed0de73ccc7b90d2276979a0
soumayagrawal1234 <145331076+soumayagrawal1234@users.noreply.github.com>
2025-05-21 10:39:45 +0530

    Update Problem-2.cpp

--- Problem-2.cpp
+++ Problem-2.cpp
@@ -1,1 +1,17 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    for (int i = 1; i <= a; i++) {
+        cout << (2 * i - 1);
+        if (i != a) {
+            cout << ", ";
+        }
+    }
+    return 0;
+}
